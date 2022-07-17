081e37e93cb445c1302edbeb9cb780e56a6a77f3
nitsan <nitsan@next-insurance.com>
2022-07-17 10:12:33 +0300

    Fix express.static

--- server/server.js
+++ server/server.js
@@ -9,20 +9,30 @@ const csrf = require('csurf');
 const app = express();
 
 // setup route middlewares
-const csrfProtection = csrf({ cookie: true })
-const parseForm = bodyParser.urlencoded({ extended: false })
+const csrfProtection = csrf({
+  cookie: true,
+  key: 'csrf',
+  secure: true,
+  maxAge: 3600,
+  httpOnly: true,
+  sameSite: 'strict'
+});
+const parseForm = bodyParser.urlencoded({extended: false})
 
 app.use(cookieParser())
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist', 'csrf')));
 
 app.get('/api/form', csrfProtection, (req, res) => {
   // pass the csrfToken to the view
-  res.json({ csrfToken: req.csrfToken() })
+  res.json({csrfToken: req.csrfToken()})
 });
-app.post('/api/update-form', parseForm, csrfProtection, (req, res) => {
+app.post('/api/update-form', csrfProtection, (req, res) => {
   res.json(req.body);
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'csrf', '/index.html'));
 });
 app.listen(3000, function () {
   console.log(`app listening on port ${3000}!`);
